d31db63e0d3e34fd16d6f372d7dbc9329ef62308
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2025-07-25 07:54:28 +0700

    wip

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import {BookIcon, ExternalLinkIcon} from "lucide-react";
+import {ExternalIcon} from "next/dist/next-devtools/dev-overlay/icons/external";
 
 /**
  * Shared layout configurations
@@ -10,19 +12,21 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        AlurKerja
-      </>
+      <img src="https://alurkerja.com/images/AlurKerja.png" width={120} alt=""/>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+  links: [
+    {
+      icon: <ExternalLinkIcon />,
+      text: 'Website',
+      url: 'https://alurkerja.com',
+      secondary: true
+    },
+    {
+      icon: <ExternalLinkIcon />,
+      text: 'Blog',
+      url: 'https://alurkerja.com/blog'
+    }
+  ],
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Inter, IBM_Plex_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const body = Inter({
   subsets: ['latin'],
+});
+
+const heading = IBM_Plex_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-plex-mono',
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={body.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
